f9d66579c4a24e0bac0aea0de91a96287ef2d145
cScholze <caleb.scholze@gmail.com>
2016-02-22 09:47:18 -0600

    added body-parser and routing for index and login pages

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// SET VIEW RENDERING ENGINE
+app.set('view engine', 'jade');
 
 // MIDDLEWARE
-app.set('view enging', 'jade');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index.jade');
+});
+
+app.post('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.jade');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
